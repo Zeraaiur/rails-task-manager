943a27cd1700000eecd6c1a60ba4726a048d0a56
David Poisson <zeraaiur@hotmail.com>
2018-05-15 17:06:36 -0400

    Its working

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,48 @@
+class TasksController < ApplicationController
+  def new
+
+  end
+
+  def create
+    @task = Task.new(user_params)
+    @task.save
+
+    redirect_to tasks_path
+  end
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(user_params)
+
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+
+  private
+
+  def user_params
+    params.require(:task).permit(:title, :details)
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # resources :tasks
+
+  # READ
+
+  get "tasks", to: "tasks#index", as: :tasks
+
+  get "task/:id", to: "tasks#show", as: :task
+
+  # NEW TASK
+
+  get "tasks/new", to: "tasks#new", as: :new_task
+
+  post "tasks", to: "tasks#create", as: :create_task
+
+  # EDIT TASK
+
+  get "task/:id/edit", to: "tasks#edit", as: :task_edit
+
+  patch "task/:id", to: "tasks#update", as: :task_update
+
+  # EDIT DELETE
+  delete "task/:id", to: "tasks#destroy", as: :delete_task
+
+
 end
